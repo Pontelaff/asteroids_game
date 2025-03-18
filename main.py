a7d3093530306152dc2aa0aac71e88dffe36f652
Jonas Gesch <jonas.gesch@gmail.com>
2025-03-18 12:37:29 +0800

    Run game at 60 FPS

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main() -> int:
     numpass, numfail = pygame.init()
     if numfail != 0:
         return -1
+    clock = pygame.time.Clock()
+    delta_t_seconds = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -18,6 +20,7 @@ def main() -> int:
                 return 0
         screen.fill((0,0,0))
         pygame.display.flip()
+        delta_t_seconds = clock.tick(FRAMES_PER_SECOND)/1000
 
 
 if __name__ == "__main__":
