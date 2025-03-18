8e73e4d7bd0046ae248b761047609938a3fbbd59
Jonas Gesch <jonas.gesch@gmail.com>
2025-03-18 13:12:41 +0800

    Add player class

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import sys
 import pygame
 from constants import *
+from player import Player
 
 def main() -> int:
     print("Starting Asteroids!")
@@ -13,12 +14,14 @@ def main() -> int:
     clock = pygame.time.Clock()
     delta_t_seconds = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 0
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         delta_t_seconds = clock.tick(FRAMES_PER_SECOND)/1000
 
