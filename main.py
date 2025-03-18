e54c61566ac146094764384a4d5b14b2d1b31f31
Jonas Gesch <jonas.gesch@gmail.com>
2025-03-18 14:16:09 +0800

    Update game state using groups

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pygame
 from constants import *
 from player import Player
 
-def init_game() -> tuple[pygame.time.Clock, pygame.Surface, Player]:
+def init_game_environment() -> tuple[pygame.time.Clock, pygame.Surface]:
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     _, numfail = pygame.init()
@@ -13,25 +13,34 @@ def init_game() -> tuple[pygame.time.Clock, pygame.Surface, Player]:
         print(f"Failed to initialize {numfail} pygame modules")
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    return clock, screen
+
+def init_game_objects() -> tuple[pygame.sprite.Group, pygame.sprite.Group]:
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
 
-    return clock, screen, player
+    return updatable, drawable
 
 def main() -> int:
-    clock, screen, player = init_game()
+    clock, screen = init_game_environment()
+    updatable, drawable = init_game_objects()
     delta_t_seconds = 0
 
+    # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 0
+
         screen.fill((0,0,0))
-        player.update(delta_t_seconds)
-        player.draw(screen)
+        updatable.update(delta_t_seconds)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         delta_t_seconds = clock.tick(FRAMES_PER_SECOND)/1000
-
-    return -1
 
 
 if __name__ == "__main__":
